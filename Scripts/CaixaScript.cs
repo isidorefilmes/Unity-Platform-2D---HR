70de1a2fc883bfcf5dd26c97f246006a76dac183
Isidore Petersen <39338469+isidorefilmes@users.noreply.github.com>
2018-10-17 17:15:43 -0300

    Add files via upload

---
+++ Scripts/CaixaScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaixaScript : MonoBehaviour {
+
+    Animator anim;
+    public float jumpForce;
+    public int diamantes;
+    public GameObject diamantePrefab;
+
+    public AudioClip[] audios;
+    AudioSource audioS;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        audioS = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            audioS.clip = audios[0];
+            audioS.Play();
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            anim.SetTrigger("Colidindo");
+            if (diamantes > 0)
+            {
+                GameObject tempDiamante = Instantiate(diamantePrefab, transform.position, transform.rotation) as GameObject;
+                tempDiamante.GetComponent<Animator>().SetTrigger("Coletando");
+                tempDiamante.GetComponent<AudioSource>().Play();
+                diamantes -= 1;
+                GameManager.gm.SetDiamantes(1);
+                Destroy(tempDiamante, 0.840f);
+            }
+            else
+            {
+                audioS.clip = audios[1];
+                audioS.Play();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+    private int vidas = 2;
+    private int diamantes = 0;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (gm == null)
+        {
+            gm = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+	}
+
+    // Update is called once per frame
+    void Start()
+    {
+        AtualizaHud();
+    }
+    void Update ()
+    {
+		
+	}
+    public void SetVidas(int vida)
+    {
+        vidas += vida;
+        if(vidas >= 0)
+        AtualizaHud();
+    }
+    public int GetVidas()
+    {
+        return vidas;
+    }
+
+    public void SetDiamantes (int diamante)
+    {
+        diamantes += diamante;
+        if (diamantes >= 50)
+        {
+            diamantes = 0;
+            vidas += 1;
+        }
+        AtualizaHud();
+    }
+
+    public void AtualizaHud()
+    {
+        GameObject.Find("VidasText").GetComponent<Text>().text = vidas.ToString();
+        GameObject.Find("DiamText").GetComponent<Text>().text = diamantes.ToString();
+    }
+
+}
