fa04395e136da0c19f50d03a5a6f0e1587b13594
Isidore Petersen <39338469+isidorefilmes@users.noreply.github.com>
2018-10-17 17:25:11 -0300

    Add files via upload

---
+++ Assets/Scripts/DiamanteScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamanteScript : MonoBehaviour {
+
+    Animator anim;
+    BoxCollider2D col;
+
+    AudioSource audioS;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        col = gameObject.GetComponent<BoxCollider2D>();
+        audioS = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            audioS.Play();
+            GameManager.gm.SetDiamantes(1);
+            col.enabled = false;
+            anim.SetTrigger("Coletando");
+            Destroy(gameObject, 0.840f);
+        }
+    }
+}
